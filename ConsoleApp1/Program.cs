8f1dde63ef03162cfbc1d9b875a642233c6b8625
Thieres Lima <thieresllima@gmail.com>
2019-12-31 10:57:31 -0300

    gerador de numeros aleatorios

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Números Aleatórios!");
+            Console.WriteLine();
+
+            Random Aleatorio = new Random();
+            Console.Write("Inicial: ");
+            int inicial = int.Parse(Console.ReadLine());
+            Console.Write("Final: ");
+            int final = int.Parse(Console.ReadLine());
+            Console.Write("Limite de numeros: ");
+            int limite = int.Parse(Console.ReadLine());
+
+            string resultado = "";
+
+            for (int i = 1; i <= limite; i++ )
+            {
+                resultado = resultado + " " + Aleatorio.Next(inicial, final);
+            }
+
+            Console.WriteLine("Números sorteados: " + resultado);
+        }
+    }
+}
